70d0081633e2ab9d4d61cfd68c8bd5acd8449b73
Dannie Vinther <davi@kea.dk>
2023-03-13 10:07:58 +0100

    js clean up

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -4,25 +4,149 @@ const classReg = /(?!\s)[^,()/*{}]+(?![^{]*})/g;
 
 sections.forEach((section) => {
   const styleTag = section.querySelector(".editor > style");
-  const _tA = section.querySelector(".editor > textarea");
+  const textarea = section.querySelector(".editor > textarea");
+  const output = section.querySelector(".output");
+  let startingCSS = textarea.innerHTML;
   const exerciseKey = section.dataset.exerciseKey;
   const boxKey = `box-${exerciseKey}`;
   let isExtra = section.dataset.extra;
-  let boxes = 0;
-  let startingCSS = _tA.innerHTML;
 
-  const output = section.querySelector(".output");
+  const boxContainer = section.querySelector(".container");
 
-  const parent = section.querySelector(".container");
-  // const resetBtns = section.querySelectorAll(".reset");
+  const addBtn = section.querySelector(".plus");
+  const removeBtn = section.querySelector(".minus");
+
+  let boxCount = boxContainer.children.length;
+  const maxBoxCount = 10;
+  const saveLocally = () => localStorage.setItem(boxKey, boxCount);
+
   const reset = section.querySelector(".reset");
   const confirming = section.querySelector(".button-group-confirm");
   const resetBtns = section.querySelector(".reset-buttons");
-  const plus = section.querySelector(".plus");
-  const minus = section.querySelector(".minus");
+
+  output.addEventListener("dblclick", (_) => {
+    output.hasAttribute("style") && output.removeAttribute("style");
+  });
+
+  function createBox(count) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.classList.add(`box-${count}`);
+    box.textContent = `box-${count}`;
+    return box;
+  }
+
+  function loadLocalStorageBoxes() {
+    const localStorageBoxes = localStorage.getItem(boxKey);
+
+    if (localStorageBoxes && localStorageBoxes !== boxCount) {
+      localStorageBoxes == 1 && localStorage.removeItem(boxKey);
+      boxCount = localStorageBoxes;
+      const newBoxes = Array.from({ length: boxCount }, (_, index) =>
+        createBox(index + 1)
+      );
+      boxContainer.replaceChildren(...newBoxes);
+      updateButtonState();
+    }
+  }
+
+  function addBox() {
+    if (boxCount == maxBoxCount) return;
+    const count = boxContainer.children.length;
+    const newBox = createBox(count + 1);
+    boxContainer.appendChild(newBox);
+
+    boxCount++;
+  }
+
+  function removeBox() {
+    if (boxCount == 1) return;
+
+    const lastBox = boxContainer.lastElementChild;
+    lastBox && boxContainer.removeChild(lastBox);
+
+    boxCount--;
+  }
+
+  function modifyBoxes(action) {
+    action();
+    saveLocally();
+    updateButtonState();
+  }
+
+  addBtn?.addEventListener("click", () => modifyBoxes(addBox));
+  removeBtn?.addEventListener("click", () => modifyBoxes(removeBox));
+
+  function setButtonDisabledState(button, isDisabled) {
+    if (!button) return;
+    button.disabled = isDisabled;
+  }
+
+  function updateButtonState() {
+    setButtonDisabledState(removeBtn, boxCount == 1);
+    setButtonDisabledState(addBtn, boxCount >= maxBoxCount);
+  }
+
+  const init = () => {
+    if (boxCount > maxBoxCount) {
+      boxCount = maxBoxCount;
+      const newBoxes = Array.from({ length: maxBoxCount }, (_, index) =>
+        createBox(index + 1)
+      );
+      boxContainer.replaceChildren(...newBoxes);
+    }
+    loadLocalStorageBoxes();
+    updateButtonState();
+
+    styleTag.innerHTML = prefix(textarea.value);
+
+    if (localStorage.getItem(exerciseKey)) {
+      textarea.value = localStorage.getItem(exerciseKey);
+      styleTag.innerHTML = prefix(textarea.value);
+    }
+
+    if (localStorage.getItem(`extra-${exerciseKey}`)) {
+      document.documentElement.dataset.extra = "true";
+    }
+
+    if (textarea.value !== startingCSS) {
+      reset.disabled = false;
+    } else {
+      reset.disabled = true;
+    }
+
+    textarea.addEventListener("input", (e) => {
+      styleTag.innerHTML = prefix(e.target.value);
+      localStorage.setItem(exerciseKey, textarea.value);
+
+      if (isExtra) {
+        localStorage.setItem(`extra-${exerciseKey}`, true);
+      }
+
+      if (textarea.value === "") {
+        localStorage.removeItem(exerciseKey);
+        localStorage.removeItem(`extra-${exerciseKey}`);
+      }
+
+      if (textarea.value !== startingCSS) {
+        reset.disabled = false;
+      } else {
+        reset.disabled = true;
+      }
+    });
+  };
+
+  init();
+
+  function prefix(str) {
+    return str.replaceAll(
+      classReg,
+      (match) => `[data-exercise-key="${exerciseKey}"] .output ${match}`
+    );
+  }
 
   reset.addEventListener("click", (e) => {
-    if (_tA.value === "") {
+    if (textarea.value === "") {
       resetUI();
     } else {
       reset.setAttribute("inert", "");
@@ -39,18 +163,18 @@ sections.forEach((section) => {
   });
 
   function resetUI() {
-    if (localStorage.getItem(exerciseKey) || _tA.value === "") {
+    if (localStorage.getItem(exerciseKey) || textarea.value === "") {
       localStorage.removeItem(exerciseKey);
       if (localStorage.getItem(`extra-${exerciseKey}`)) {
         localStorage.removeItem(`extra-${exerciseKey}`);
       }
       if (localStorage.getItem(boxKey)) {
         localStorage.removeItem(boxKey);
-        boxes = 0;
+        boxCount = boxContainer.children.length;
       }
       styleTag.innerHTML = null;
-      _tA.value = startingCSS;
-      _tA.focus(); /* ensure that the UI updates */
+      textarea.value = startingCSS;
+      textarea.focus(); /* ensure that the UI updates */
       reset.disabled = true;
       output.getAttribute("style") && output.removeAttribute("style");
       init();
@@ -58,201 +182,39 @@ sections.forEach((section) => {
       reset.disabled = false;
     }
   }
-
-  output.addEventListener("dblclick", (_) => {
-    output.hasAttribute("style") && output.removeAttribute("style");
-  });
-
-  function prefix(str) {
-    return str.replaceAll(
-      classReg,
-      (match) => `[data-exercise-key="${exerciseKey}"] .output ${match}`
-    );
-  }
-
-  function addButtonListeners() {
-    if (!plus && !minus) return;
-
-    if (localStorage.getItem(boxKey)) {
-      boxes = localStorage.getItem(boxKey);
-    }
-
-    minus.disabled = boxes > 0 ? false : true;
-
-    plus.addEventListener("click", (e) => {
-      addBox();
-
-      boxes++;
-
-      minus.disabled = false;
-
-      boxCount();
-    });
-
-    minus.addEventListener("click", (e) => {
-      removeBox();
-
-      minus.disabled = parent.children.length <= 1 ? true : false;
-
-      if (boxes === 0) return;
-      boxes--;
-
-      boxCount();
-    });
-  }
-
-  addButtonListeners();
-
-  function addBox() {
-    const count = parent.children.length;
-    const box = document.createElement("div");
-    box.className = `box box-${count + 1}`;
-    box.textContent = `.box-${count + 1}`;
-    parent.append(box);
-  }
-
-  function addClasses() {
-    [...parent.children].forEach((b, i) => {
-      b.className = `box box-${i + 1}`;
-    });
-  }
-
-  function removeBox() {
-    if (parent.children.length - 1) {
-      const last = parent.lastElementChild;
-      parent.removeChild(last);
-    }
-  }
-
-  function boxCount() {
-    if (boxes < 0) return;
-    localStorage.setItem(boxKey, boxes);
-  }
-
-  addClasses();
-
-  const init = () => {
-    styleTag.innerHTML = prefix(_tA.value);
-
-    if (localStorage.getItem(exerciseKey)) {
-      _tA.value = localStorage.getItem(exerciseKey);
-      styleTag.innerHTML = prefix(_tA.value);
-    }
-
-    if (localStorage.getItem(`extra-${exerciseKey}`)) {
-      document.documentElement.dataset.extra = "true";
-    }
-
-    if (localStorage.getItem(boxKey)) {
-      boxes = localStorage.getItem(boxKey);
-      for (let i = 0; i < boxes; i++) {
-        addBox(i);
-      }
-    }
-
-    if (_tA.value !== startingCSS) {
-      reset.disabled = false;
-    } else {
-      reset.disabled = true;
-    }
-
-    _tA.addEventListener("input", (e) => {
-      styleTag.innerHTML = prefix(e.target.value);
-      localStorage.setItem(exerciseKey, _tA.value);
-
-      if (isExtra) {
-        localStorage.setItem(`extra-${exerciseKey}`, true);
-      }
-
-      if (_tA.value === "") {
-        localStorage.removeItem(exerciseKey);
-        localStorage.removeItem(`extra-${exerciseKey}`);
-      }
-
-      if (_tA.value !== startingCSS) {
-        reset.disabled = false;
-      } else {
-        reset.disabled = true;
-      }
-    });
-  };
-
-  init();
 });
 
-// const downloadBtn = document.querySelector("#download");
+// const keySequence = [];
+// let konamiString = "";
+// const konamiCode = [
+//   "ArrowUp",
+//   "ArrowUp",
+//   "ArrowDown",
+//   "ArrowDown",
+//   "ArrowLeft",
+//   "ArrowRight",
+//   "ArrowLeft",
+//   "ArrowRight",
+//   "b",
+//   "a",
+// ];
 
-// downloadBtn.addEventListener("click", (_) => {
-//   const array = [];
-
-//   let keys = Object.keys(localStorage);
-//   for (let key of keys) {
-//     array.push(`Exercise ${key}:\n${localStorage.getItem(key)}`);
-//   }
-//   const copy = [...array].filter((el) => !el.includes("box")).join("\n\n");
-//   download("exercises.txt", copy);
-// });
-
-// function download(filename, text) {
-//   const element = document.createElement("a");
-//   element.setAttribute(
-//     "href",
-//     "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+// document.addEventListener("keyup", function (e) {
+//   keySequence.push(e.key);
+//   keySequence.splice(
+//     -konamiCode.length - 1,
+//     keySequence.length - konamiCode.length
 //   );
-//   element.setAttribute("download", filename);
+//   konamiString = konamiCode.join("");
 
-//   element.style.display = "none";
-//   document.body.appendChild(element);
+//   if (
+//     keySequence.join("").includes(konamiString) &&
+//     !document.documentElement.dataset.extra
+//   ) {
+//     document.documentElement.dataset.extra = "true";
 
-//   element.click();
-
-//   document.body.removeChild(element);
-// }
-
-// const footerBtn = document.querySelector(".dbclick");
-
-// footerBtn.addEventListener("dblclick", (e) => {
-//   document.documentElement.dataset.extra = "true";
-//   document
-//     .querySelectorAll("section[data-extra='true']")[0]
-//     .scrollIntoView({ behavior: "smooth" });
+//     document
+//       .querySelectorAll("section[data-extra='true']")[0]
+//       .scrollIntoView({ behavior: "smooth" });
+//   }
 // });
-
-// This code creates a key sequence array, and then uses that array to compare
-// the user's input to the Konami code. If the user enters the correct sequence,
-// it scrolls the page to the hidden section.
-
-const keySequence = [];
-let konamiString = "";
-const konamiCode = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a",
-];
-
-document.addEventListener("keyup", function (e) {
-  keySequence.push(e.key);
-  keySequence.splice(
-    -konamiCode.length - 1,
-    keySequence.length - konamiCode.length
-  );
-  konamiString = konamiCode.join("");
-
-  if (
-    keySequence.join("").includes(konamiString) &&
-    !document.documentElement.dataset.extra
-  ) {
-    document.documentElement.dataset.extra = "true";
-
-    document
-      .querySelectorAll("section[data-extra='true']")[0]
-      .scrollIntoView({ behavior: "smooth" });
-  }
-});
