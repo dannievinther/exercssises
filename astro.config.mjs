633a442207f4d21114408bc4d61996df6e0a4c3c
Dannie Vinther <davi@kea.dk>
2023-03-18 13:47:04 +0100

    new exercise imgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import compress from "astro-compress";
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     server: {
-      host: "0.0.0.0",
+      host: "0.0.0.0"
     },
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
-  integrations: [compress()],
+  integrations: [compress(), image()]
 });
